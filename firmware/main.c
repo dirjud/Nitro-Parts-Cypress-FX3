6d1f4a24b80b712e5f7d1619f9c01e269beebd99
Dennis Muhlestein <djmuhlestein@gmail.com>
2013-04-25 19:34:52 -0600

    Tmp drive v18_en low to avoid sdram problem.

--- firmware/main.c
+++ firmware/main.c
@@ -144,6 +144,7 @@ void init_gpio (void) {
     }
    
     // drive V18_EN high
+    gpioConfig.outValue = CyFalse;
     apiRetStatus = CyU3PGpioSetSimpleConfig(27, &gpioConfig);
     if (apiRetStatus != CY_U3P_SUCCESS) {
       /* Error handling */
