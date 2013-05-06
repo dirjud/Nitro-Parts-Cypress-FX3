bafb398b2ddae0380eeac534d4d0fb6d707343d2
Dennis Muhlestein <djmuhlestein@gmail.com>
2013-05-06 17:10:48 -0600

    Auto generate Cypress copyrighted files.

--- firmware/nitrofx3.mk
+++ firmware/nitrofx3.mk
@@ -6,18 +6,20 @@ DEPS = fx3_terminals.h
 include config.mk
 
 BUILDDIR ?= build
+GENDIR ?= gen
 
 
-SOURCE_ABS := $(abspath $(SOURCE))
+SOURCE_ABS := $(abspath $(SOURCE) $(GENDIR)/cyfxtx.c )
 C_OBJECT := $(addprefix $(BUILDDIR), $(SOURCE_ABS:%.c=%.o))
 SOURCE_ASM_ABS := $(abspath $(SOURCE_ASM))
 A_OBJECT:= $(addprefix $(BUILDDIR), $(SOURCE_ASM_ABS:%.S=%.o))
+CYFXTX_C := $(abspath $(GENDIR)/cyfxtx.c)
 
 EXES = $(MODULE).$(EXEEXT)
 
 
 # the common include path
-Include	+= -I$(CYFX3SDK)/firmware/u3p_firmware/inc -I. -I$(FX3DIR) -I../../../Microchip/M24XX/fx3 -I../../../Xilinx/Spartan/fx3
+Include	+= -I$(CYFX3SDK)/firmware/u3p_firmware/inc -I. -I$(FX3DIR) -I$(GENDIR) -I../../../Microchip/M24XX/fx3 -I../../../Xilinx/Spartan/fx3
 
 # the common compiler options
 CCFLAGS += -O0 \
@@ -45,7 +47,7 @@ ASMFLAGS = -Wall -c -mcpu=arm926ej-s -mthumb-interwork -O0 -lgcc
 
 CCFLAGS	 += -Wall -mcpu=arm926ej-s -mthumb-interwork -O0 -lgcc
 
-LDFLAGS	 += -T $(FX3DIR)fx3.ld -d --no-wchar-size-warning -Map $(MODULE).map
+LDFLAGS	 += -T $(GENDIR)/fx3.ld -d --no-wchar-size-warning -Map $(MODULE).map
 
 #The ARM toolchain location and the version are taken from environment variables
 LDLIBS  += \
@@ -64,6 +66,16 @@ GEN_IMAGE       = $(ELF2IMG)  -i $< -o $@ -imgtype 0xB0 -i2cconf 0x1E
 
 all:fx3_terminals.h compile
 
+$(CYFXTX_C): $(CYFX3SDK)/firmware/common/cyfxtx.c config.mk
+	mkdir -p $(GENDIR)
+	cat $< | sed -e 's/0x40038000/0x4002E000/' \
+			     -e 's/0x40078000/0x40040000/' > $@
+
+$(GENDIR)/fx3.ld: $(CYFX3SDK)/firmware/common/fx3.ld config.mk
+	mkdir -p $(GENDIR)
+	cat $< | sed -e 's/0x2D000/0x23000/' \
+			     -e 's/0x40030000/0x40026000/' > $@
+
 ELF2IMG:
 	# not this needs fixed
 	make -C $(CYFX3SDK)/util/elf2img
@@ -71,7 +83,7 @@ ELF2IMG:
 $(MODULE).img: $(MODULE).$(EXEEXT)
 	$(GEN_IMAGE)
 
-$(MODULE).$(EXEEXT): $(A_OBJECT) $(C_OBJECT)
+$(MODULE).$(EXEEXT): $(A_OBJECT) $(C_OBJECT) $(GENDIR)/fx3.ld
 	$(LINK)
 
 $(C_OBJECT) : $(BUILDDIR)/%.o : /%.c
@@ -87,11 +99,14 @@ clean:
 	rm -f $(MODULE).$(EXEEXT)
 	rm -f $(MODULE).map
 	rm -f $(A_OBJECT) $(C_OBJECT)
-	rm -f vidpid.h
+	rm -f $(GENDIR)/vidpid.h
+	rm -f $(GENDIR)/cyfxtx.c
+	rm -f $(GENDIR)/fx3.ld
 
-compile: vidpid.h $(C_OBJECT) $(A_OBJECT) $(EXES) $(MODULE).img
+compile: $(GENDIR)/vidpid.h $(C_OBJECT) $(A_OBJECT) $(EXES) $(MODULE).img
 
-vidpid.h: config.mk
+$(GENDIR)/vidpid.h: config.mk
+	@mkdir -p $(GENDIR)
 	@echo "#define VID $(VID)" > $@
 	@echo "#define PID $(PID)" >> $@
 
