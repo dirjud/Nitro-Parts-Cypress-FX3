772eeaac8d81bb1726e4a8428f673ad7a16b36f4
Dennis Muhlestein <djmuhlestein@gmail.com>
2014-04-10 07:39:53 -0600

    Updated script.

--- py/start.py
+++ py/start.py
@@ -1,8 +1,4 @@
-import nitro_parts.Cypress.fx3, logging, numpy
-from nitro_parts.BrooksEE.FX3DEV import speed
-
+import nitro_parts.Cypress.fx3, logging
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 dev = self = nitro_parts.Cypress.fx3.get_dev()
-
-
