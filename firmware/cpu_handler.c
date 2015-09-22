0b258303505e4d64a5046078326482879c6daa5e
Dennis Muhlestein <djmuhlestein@gmail.com>
2015-09-22 08:44:14 -0600

    Correct cpu handler read size to match burst settings.

--- firmware/cpu_handler.c
+++ firmware/cpu_handler.c
@@ -196,10 +196,7 @@ CyU3PReturnStatus_t cpu_handler_setup(void) {
        * so that a full burst can be completed.  This will mean that a
        * buffer will be available only after it has been filled or when a
        * short packet is received. */
-      dmaCfg.size  = gRdwrCmd.ep_buffer_size; //* CY_FX_EP_BURST_LENGTH;
-      /* Multiply the buffer size with the multiplier for performance
-       * improvement. */
-      dmaCfg.size *= CY_FX_DMA_SIZE_MULTIPLIER;
+      dmaCfg.size      = gRdwrCmd.ep_buffer_size * CY_FX_EP_BURST_LENGTH * CY_FX_DMA_SIZE_MULTIPLIER;
       dmaCfg.count     = CY_FX_EP_BUF_COUNT;
       dmaCfg.prodSckId = CY_FX_EP_PRODUCER_SOCKET;
       dmaCfg.consSckId = CY_U3P_CPU_SOCKET_CONS;
@@ -220,7 +217,7 @@ CyU3PReturnStatus_t cpu_handler_setup(void) {
       /* Create a DMA MANUAL_OUT channel for the consumer socket. */
       dmaCfg.prodSckId = CY_U3P_CPU_SOCKET_PROD;
       dmaCfg.consSckId = CY_FX_EP_CONSUMER_SOCKET;
-      dmaCfg.size *= CY_FX_EP_BURST_LENGTH; // NOTE only enough memory for IN ep to have burst
+      dmaCfg.size  = gRdwrCmd.ep_buffer_size * CY_FX_DMA_SIZE_MULTIPLIER; 
       apiRetStatus = CyU3PDmaChannelCreate (&glChHandleBulkSrc, CY_U3P_DMA_TYPE_MANUAL_OUT, &dmaCfg);
       if (apiRetStatus != CY_U3P_SUCCESS) {
         log_error("CyU3PDmaChannelCreate failed, Error code = %d\n", apiRetStatus);
