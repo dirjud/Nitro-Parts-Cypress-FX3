d85682fe99e3d12642d22baa7ae96dc309803afb
Dennis Muhlestein <djmuhlestein@gmail.com>
2016-02-09 12:07:05 -0700

    bug in uxn1330 board requires not using low power mode.

--- firmware/low_power.h
+++ firmware/low_power.h
@@ -4,8 +4,8 @@
 #define FX3_LP_B 26
 
 // set this to CyFalse to enable low voltage mode. Setting to CyTrue
-// effectively disables low voltage mode
-#define LP_B_INITIAL CyFalse
+// effectively disables low voltage mode at startup
+#define LP_B_INITIAL CyTrue
 
 #define ENTER_LOW_VOLTAGE_MODE  do { CyU3PGpioSetValue ( FX3_LP_B, LP_B_INITIAL ); } while (0)
 #define ENTER_HIGH_VOLTAGE_MODE do { CyU3PGpioSetValue ( FX3_LP_B, CyTrue); } while (0)
