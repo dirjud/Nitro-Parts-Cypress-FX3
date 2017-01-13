a39d355129b8409a35865aec4a92578d619c2a55
Dennis Muhlestein <djmuhlestein@gmail.com>
2017-01-13 11:24:15 -0700

    1330 specific functions moving to 1330 repo.

--- firmware/fx3_term.c
+++ firmware/fx3_term.c
@@ -10,14 +10,6 @@ extern rdwr_cmd_t gRdwrCmd;
 extern const uint8_t CyFxUSB30DeviceDscr[];
 
 
-fx3_gpif_config_t fx3_gpif_config = {
-    .gpif_clk_div=8,
-    .gpif_clk_halfdiv=CyFalse,
-    .gpif_clk_src= CY_U3P_SYS_CLK,
-    .gpif_drive_strength = CY_U3P_DS_QUARTER_STRENGTH,
-    .enable = CyTrue,
-};
-
 uint16_t fx3_read(CyU3PDmaBuffer_t* pBuf) {
     uint16_t ret;
     switch (gRdwrCmd.header.reg_addr) {
@@ -30,21 +22,6 @@ uint16_t fx3_read(CyU3PDmaBuffer_t* pBuf) {
        case FX3_USB3:
         ret=gRdwrCmd.ep_buffer_size == 1024 ? 1 : 0;
         break;
-       case FX3_GPIF_CLK_DIV:
-        ret=fx3_gpif_config.gpif_clk_div; 
-        break;
-       case FX3_GPIF_CLK_HALFDIV:
-        ret=fx3_gpif_config.gpif_clk_halfdiv;
-        break;
-       case FX3_GPIF_CLK_SRC:
-        ret=fx3_gpif_config.gpif_clk_src;
-        break;
-       case FX3_GPIF_DRIVE_STRENGTH:
-        ret=fx3_gpif_config.gpif_drive_strength;
-        break;
-       case FX3_GPIF_ENABLE:
-        ret=fx3_gpif_config.enable;
-        break;
        default:
            return 1;
     }
@@ -53,36 +30,7 @@ uint16_t fx3_read(CyU3PDmaBuffer_t* pBuf) {
 
 }
 
-extern void slfifo_init();
 
 uint16_t fx3_write(CyU3PDmaBuffer_t* pBuf) {
-    uint16_t val = pBuf->buffer[0] | (pBuf->buffer[1]<<8);
-    switch ( gRdwrCmd.header.reg_addr) {
-        case FX3_GPIF_CLK_DIV:
-            if (val<2 ||val>1024) return 1; 
-            fx3_gpif_config.gpif_clk_div = val; 
-            break;
-        case FX3_GPIF_CLK_HALFDIV:
-            fx3_gpif_config.gpif_clk_halfdiv = val>0?CyTrue:CyFalse;
-            break;
-        case FX3_GPIF_CLK_SRC:
-            if (val>3) return 1;
-            fx3_gpif_config.gpif_clk_src=val;
-            break;
-        case FX3_GPIF_CLK_TRIGGER:
-            CyU3PPibDeInit();
-            slfifo_init();
-            break;
-        case FX3_GPIF_DRIVE_STRENGTH:
-            fx3_gpif_config.gpif_drive_strength=val;
-            CyU3PSetPportDriveStrength(val);
-            break;
-        case FX3_GPIF_ENABLE:
-            fx3_gpif_config.enable=val;
-            break;
-        default:
-            return 1;
-    }
-    
-    return 0;
+    return 1;
 }
--- firmware/fx3_term.h
+++ firmware/fx3_term.h
@@ -6,16 +6,6 @@
 uint16_t fx3_read(CyU3PDmaBuffer_t*);
 uint16_t fx3_write(CyU3PDmaBuffer_t*);
 
-typedef struct {
-  int gpif_clk_div;
-  CyBool_t gpif_clk_halfdiv;
-  int gpif_clk_src;
-  int gpif_drive_strength;
-  CyBool_t enable;
-} fx3_gpif_config_t;
-
-extern fx3_gpif_config_t fx3_gpif_config;
-
 #define DECLARE_FX3_HANDLER(term) \
     DECLARE_HANDLER(&glCpuHandler,term,0,0,fx3_read,fx3_write,0,0,0,0)
 
